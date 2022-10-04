4c0cc6d869e38e5bc38a165e6fd62c622ea5efc9
sora_uzu <naruto07170717@gmail.com>
2022-10-04 20:33:55 +0900

    #2 init react app

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react'
+  ],
+  "ignorePatterns": [
+    ".eslintrc.js"
+  ],
+  rules: {
+    'no-use-before-define': "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    'import/prefer-default-export': "off",
+    'import/extensions': [
+        'error',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.jsx', '.tsx'],
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'no-void': [
+        'error',
+        {
+          allowAsStatement: true,
+        },
+      ],
+      "react/function-component-definition": [
+        2,
+        {
+          namedComponents: "arrow-function",
+        },
+      ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      },
+    },
+  },
+};
