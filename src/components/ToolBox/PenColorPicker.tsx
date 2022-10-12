4d10280fee2eb1ee5fa71e626351bf96aa27205e
sora_uzu <naruto07170717@gmail.com>
2022-10-12 20:37:53 +0900

    #10add color picker button

--- src/components/ToolBox/PenColorPicker.tsx
+++ src/components/ToolBox/PenColorPicker.tsx
@@ -1,15 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SketchPicker } from "react-color";
 import { KonvaContext } from "../../context/KonvaContext";
 
 const PenColorPicker = () => {
   const { penColor, setPenColor } = useContext(KonvaContext);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const handleChange = (color: { hex: string }): void => {
     setPenColor(color.hex);
   };
 
-  return <SketchPicker color={penColor} onChange={handleChange} />;
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div>
+        <div>
+          <button type="button" onClick={handleOpen}>
+            <div>カラーを選択する</div>
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+        <div>
+          <button type="button" onClick={handleClose}>
+            <div>閉じる</div>
+          </button>
+          <SketchPicker color={penColor} onChange={handleChange} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default PenColorPicker;
