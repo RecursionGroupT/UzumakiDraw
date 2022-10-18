a6eabb6785bda0f198e93609bf1af7afd1ab0476
sora_uzu <naruto07170717@gmail.com>
2022-10-19 00:56:42 +0900

    #21 add timer

--- src/pages/DrawPage.tsx
+++ src/pages/DrawPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ToolBox from "../components/ToolBox/ToolBox";
 import KonvaCanvas from "../components/KonvaCanvas";
+import MyTimer from "../components/DrawPage/Timer";
 
 const DrawPage = () => (
   <>
+    <MyTimer />
     <ToolBox />
     <KonvaCanvas />
   </>
