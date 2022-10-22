a27dfbb21da7797db4576abfa2e6b82417200b6b
Jun Cho <strelitzia2692@gmail.com>
2022-10-22 23:37:36 +0900

    #27 fix conflicts

--- src/components/DrawPage/SubjectDisplay.tsx
+++ src/components/DrawPage/SubjectDisplay.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const SubjectDisplay: React.FC<Props> = ({ subject }) => (
-  <div className=" rounded-t-md border-4 border-black bg-gray-900 p-6 text-center text-xl font-bold text-white">
+  <div className="rounded-t-md border-4 border-black bg-gray-900 p-6 text-center text-xl font-bold text-white">
     <div>{subject}</div>
   </div>
 );
